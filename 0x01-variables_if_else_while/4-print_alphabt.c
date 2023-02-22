de770d970958e76c783caedb52ad3c6ea9a348a9
falama festus <falama180@gmail.com>
2023-02-22 03:46:46 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+@@ -1,15 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This program prints the alphabet in lowercase
+ * but skips the letters q and e, using only putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
