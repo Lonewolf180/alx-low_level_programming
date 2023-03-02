782d2d0ad2e87d0b6ced84a0e128913d528b1fb2
falama festus <falama180@gmail.com>
2023-03-02 03:18:28 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,37 @@
-#include "main.h"
+#include &quot;main.h&quot;
 
 /**
- * main - This program prints the phrase Hello, World
- * Return: Always 0 (Success)
- */
+
+* main - print _putchar
+
+* is written by mbah
+
+* Return: 0
+
+*/
+
 int main(void)
+
 {
-	char ch[] = "main";
-	int i;
 
-	for (i = 0; ch[i] != '\0'; i++)
-	{
-		_putchar(ch[i]);
-	}
+_putchar(&#39;_&#39;);
 
-	_putchar('\n');
-	return (0);
+_putchar(&#39;p&#39;);
+
+_putchar(&#39;u&#39;);
+
+_putchar(&#39;t&#39;);
+
+_putchar(&#39;c&#39;);
+
+_putchar(&#39;h&#39;);
+
+_putchar(&#39;a&#39;);
+
+_putchar(&#39;r&#39;);
+
+_putchar(&#39;\n&#39;);
+
+return (0);
+
 }
